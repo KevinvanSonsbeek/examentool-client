8f7b6992453237882a1a05db030ef8128675576d
jopdewinter2000 <jopdewinter2000@gmail.com>
2018-10-03 14:20:40 +0200

    Created home page with exams

---
+++ src/components/DeterminedExams.index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="DeterminedExams">
+    <div v-for="Exam in DeterminedExams" class="determinedExam">
+      <p>{{ Exam.exam_title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'DeterminedExam',
+        data () {
+            return {
+                DeterminedExams: []
+            }
+        },
+        created () {
+            this.$http.get('http://localhost:8000/exams/full').then(function(data) {
+                console.log(data)
+                this.DeterminedExams = data.body
+            })
+        }
+    }
+</script>
---
+++ src/views/DeterminedExams.index.vue
@@ -0,0 +1,15 @@
+<template>
+  <DeterminedExams></DeterminedExams>
+</template>
+
+<script>
+// @ is an alias to /src
+import DeterminedExams from '@/components/DeterminedExams.index.vue'
+
+export default {
+  name: 'DeterminedExam',
+  components: {
+    DeterminedExams
+  }
+}
+</script>
