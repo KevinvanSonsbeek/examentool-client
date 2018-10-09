c50d809bbb4324d09563b46266f34b2100d53702
Richard Boon <mail@richardboon.nl>
2018-10-09 13:17:57 +0200

    Logic for new assessment

--- src/router.js
+++ src/router.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import DeterminedExams from './views/DeterminedExams.index.vue'
 import DeterminedExamsAssessment from './views/DeterminedExams.assessment.vue'
+import DeterminedExamsNewAssessment from './views/DeterminedExams.newAssessment.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -15,9 +16,14 @@ export default new Router({
         component: DeterminedExams
       },
       {
-        path: '/exams/:examId/assessment',
-        name: 'DeterminedExamsAssessment',
-        component: DeterminedExamsAssessment
+        path: '/assessment/:examId/new',
+        name: 'DeterminedExamsNewAssessment',
+        component: DeterminedExamsNewAssessment
+      },
+      {
+          path: '/assessment/:examId',
+          name: 'DeterminedExamsAssessment',
+          component: DeterminedExamsAssessment
       }
   ]
 })
