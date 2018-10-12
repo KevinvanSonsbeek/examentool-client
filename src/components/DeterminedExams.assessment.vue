9cdc9a6327a6674bdd05c1e4cec76fdca0129178
jopdewinter2000 <jopdewinter2000@gmail.com>
2018-10-12 09:29:30 +0200

    styled the sections table

--- src/components/DeterminedExams.assessment.vue
+++ src/components/DeterminedExams.assessment.vue
@@ -14,30 +14,36 @@
                 <td>Examinator:</td>
                 <td><input placeholder="J. Slemmer" type="text"></td>
               </tr>
-              <button id="infoContinue" v-on:click="StartAssignment" type="button" class="btn btn-primary">Volgende</button>
+              <button id="infoContinue" v-on:click="startAssignment" type="button" class="btn btn-primary">Volgende</button>
           </tbody>
         </table>
-        <div id="sectionsDiv">
-            <table class="table sectionTable">
+
+          <table id="sectionTables" class="table sectionTable">
             <tbody class="" v-for="section in sections"><br><br>
             <tr>
                 <td class="sectionHeader">{{ section.title }}</td>
-                </tr>
-                <tr>
+              </tr>
+              <tr>
                 <td>Vraag:</td>
                 <td>Wel:</td>
                 <td>Niet:</td>
                 <td>Twijfel:</td>
-                </tr>
-                <tr v-for="criteria in section.criteria">
-                    <td v-bind:id="criteria.criteria_name + 'Title'">{{ criteria.criteria_name }}</td>
-                    <td><input class="form-check-input" v-on:change="SaveStorage('radio', criteria.criteria_name, 'wel')" v-bind:name="criteria.criteria_name" v-bind:id="criteria.criteria_name + 'wel'" type="radio" value="option1"></td>
-                    <td><input class="form-check-input" v-on:change="SaveStorage('radio', criteria.criteria_name, 'niet')" v-bind:name="criteria.criteria_name" v-bind:id="criteria.criteria_name + 'niet'" type="radio" value="option2"></td>
-                    <td><input class="form-check-input" v-on:change="SaveStorage('checkbox', criteria.criteria_name + 'Doubt', 'doubt')" v-bind:id="criteria.criteria_name + 'Doubt'" type="checkbox" value=""></td>
-                </tr>
+                <td>Uitleg:</td>
+                <td>Notitie:</td>
+              </tr>
+              <tr v-for="criteria in section.criteria">
+                <td>{{ criteria.criteria_name }}</td>
+                <td><input class="form-check-input" v-bind:name="criteria.criteria_name" id="wel" type="radio" value="option1"></td>
+                <td><input class="form-check-input" v-bind:name="criteria.criteria_name" id="niet" type="radio" value="option2"></td>
+                <td><input class="form-check-input" type="checkbox" value=""></td>
+                <td><button v-on:click="showInfo(criteria.criteria_description)">?</button></td>
+                <td></td>
+              </tr>
             </tbody>
-            </table>
-        </div>
+          </table>
+
+          <div id="cardDiv"></div>
+          
     </div>
 </template>
 
@@ -54,15 +60,13 @@ export default {
   // Function called at creation of the page
   created() {
     // API call
-
-
     this.$http
       .get("http://localhost:8000/exam/" + this.$route.params.examId + "/start")
       .then(
         response => {
           // Succeed
           this.sections = response.body.exam_criteria;
-          console.log(response);
+          console.log(this.sections);
         },
         response => {
           // Failed
@@ -77,50 +81,11 @@ export default {
       );
   },
   methods: {
-    StartAssignment: function() {
-        document.getElementById("infoTable").style.display = "none";
-        document.getElementById("sectionsDiv").style.display = "block";
-
-        for(var section in this.sections)
-        {
-            for(var curCriteria in this.sections[section].criteria)
-            {
-                var criteriaName = this.sections[section].criteria[curCriteria].criteria_name;
-                
-                if(localStorage.getItem(criteriaName) == "null")
-                {
-
-                }else if(localStorage.getItem(criteriaName) == "wel")
-                {
-                    document.getElementById(criteriaName + "wel").checked = true;
-                }else if(localStorage.getItem(criteriaName) == "niet")
-                {
-                    document.getElementById(criteriaName + "niet").checked = true;
-                }
-                if(localStorage.getItem(criteriaName + "Doubt") == "null")
-                {
-                    
-                }else if(localStorage.getItem(criteriaName + "Doubt") == "checked")
-                {
-                    document.getElementById(criteriaName + "Doubt").checked = true;
-                }
-            }
-        }
+    startAssignment: function() {
+      document.getElementById("infoTable").hidden = true;
+      document.getElementById("sectionTables").style.display = "block";
     },
-    SaveStorage: function(type, string, status) {
-        if(type == "radio"){
-            localStorage.setItem(string, status);
-        }else if(type == "checkbox")
-        {
-            if(document.getElementById(string).checked == true)
-            {
-                localStorage.setItem(string, "checked");
-            }else if(document.getElementById(string).checked == false)
-            {
-                localStorage.setItem(string, "notChecked")
-            }
-        }
-    }
+    
   }
 };
 </script>
@@ -129,12 +94,11 @@ export default {
   #infoTable {
     width: 600px;
     margin: auto;
-    /* display: none; */
   }
   #infoTable input {
     width: 200px;
   }
-  #sectionsDiv {
+  #sectionTables{
     display: none;
   }
   .sectionTable {
@@ -143,5 +107,15 @@ export default {
   }
   .sectionHeader {
     background: lightgrey;
+    width: 100%;
+  }
+  .card {
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    padding: 20px;
+    min-width: 400px;
+    margin: 0px auto;
+    vertical-align: middle!important;
+    background: white;
   }
 </style>
